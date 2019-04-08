d8fd1fc7b9c742ff94ebdffbeebeedf423590000
Abolfazl Ahmadi rahmat <38430894+ahmadiphy@users.noreply.github.com>
2019-04-09 01:07:28 +0430

    Update test.py

--- test.py
+++ test.py
@@ -5,15 +5,11 @@ cwd = os.getcwd()
 
 from MCKLD import Rel_ent,Exp_rel_ent
 
-RE=Rel_ent(cwd + '/REdata/chain.out',cwd + '/REdata/chainprob.out',cwd + '/REdata/lnprior.out')
-re=RE.Run()
-print(re)
-
-M_path=cwd + '/M.txt'
-def model_fun(theta):
-     M = np.genfromtxt(M_path)
-     return M@theta
-
-ERE=Exp_rel_ent(cwd+'/EREdata/D1_sample.txt',cwd+'/EREdata/C.txt',1000,10)
-ere=ERE.Run(model_fun)
-print(ere)
+#RE=Rel_ent(cwd + '/REdata/chain.out',cwd + '/REdata/chainprob.out',cwd + '/REdata/lnprior.out')
+#re=RE.Run()
+#print(re)
+functions_path=cwd+'/Functions.py'
+ERE=Exp_rel_ent(cwd+'/EREdata/D1_sample.txt',cwd+'/EREdata/C.txt',10,300)
+#ere=ERE.Run()
+#print(ere)
+pere=ERE.PRun(2)
