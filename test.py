4af1942803d482280a30ab1ebe0bc2d6bbd96626
Abolfazl Ahmadi rahmat <38430894+ahmadiphy@users.noreply.github.com>
2019-04-18 16:29:58 +0430

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ cwd = os.getcwd()
 
 from MCKLD import Rel_ent,Exp_rel_ent
 
-RE=Rel_ent(cwd + '/REdata/chain.out',cwd + '/REdata/lnprior.out')
+RE=Rel_ent(cwd + '/REdata/chain.out',cwd + '/REdata/lnprior.out',False)
 re=RE.Run()
 print(re)
-#functions_path=cwd+'/Functions.py'
-#ERE=Exp_rel_ent(cwd+'/data/D1_sample.txt',cwd+'/data/C.txt',cwd+'/Functions.py',10)
-#ere=ERE.Run(300)
-#print('ere is:')
-#print(ere)
+functions_path=cwd+'/Functions.py'
+ERE=Exp_rel_ent(cwd+'/EREdata/D1_sample.txt',cwd+'/EREdata/C.txt',cwd+'/Functions.py',10)
+ere=ERE.Run(300)
+print('ere is:')
+print(ere)
 #pere=ERE.PRun(2,300)
