541289abafdc704e57c9461447ab7beced54c893
Abolfazl Ahmadi rahmat <38430894+ahmadiphy@users.noreply.github.com>
2019-04-18 16:43:34 +0430

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,19 @@ import numpy as np
 import sys
 import os
 cwd = os.getcwd()
+import tarfile
 
 from MCKLD import Rel_ent,Exp_rel_ent
 
+tf1 = tarfile.open(cwd+'REdata.tar.xz')
+tf1.extractall()
 RE=Rel_ent(cwd + '/REdata/chain.out',cwd + '/REdata/lnprior.out',False)
 re=RE.Run()
 print(re)
+
+
+tf2 = tarfile.open(cwd+'EREdata.tar.xz')
+tf2.extractall()
 functions_path=cwd+'/Functions.py'
 ERE=Exp_rel_ent(cwd+'/EREdata/D1_sample.txt',cwd+'/EREdata/C.txt',cwd+'/Functions.py',10)
 ere=ERE.Run(300)
