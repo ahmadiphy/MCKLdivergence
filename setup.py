cfad12dfaf16914d8474a311fdf7e03266a6cd36
Abolfazl Ahmadi-Rahmat <38430894+ahmadiphy@users.noreply.github.com>
2020-11-03 14:57:05 +0330

    Update setup.py
    PEP 8

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 try:
     from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 
 setup(
-	name='MCKLD',
-	version='0.1',
-	description='estimate the relative entropy',
-	long_description_content_type="text/markdown",
-	authors='Ahmad Mehrabi, Abolfazl Ahmadi Rahmat',
-	url='https://github.com/ahmadiphy/MCKLdivergence',
-	packages=[],
-	scripts=['MCKLD.py','MCKLD_parallelERE.py'],
-	install_requires=['MCEvidence'],
+	name = 'MCKLD',
+	version = '0.1',
+	description = 'estimate the relative entropy',
+	long_description_content_type = "text/markdown",
+	authors = 'Ahmad Mehrabi, Abolfazl Ahmadi Rahmat',
+	url = 'https://github.com/ahmadiphy/MCKLdivergence',
+	packages = [],
+	scripts = ['MCKLD.py','MCKLD_parallelERE.py'],
+	install_requires = ['MCEvidence'],
 	)
