d3ebe993add70f6fb97813c460fdcbe8d42bcc21
Abolfazl Ahmadi-Rahmat <38430894+ahmadiphy@users.noreply.github.com>
2020-11-17 22:05:52 +0330

    Update example.py

--- Examples/Expected_relative_entropy/example.py
+++ Examples/Expected_relative_entropy/example.py
@@ -7,13 +7,13 @@ from MCKLD import Exp_rel_ent
 
 
 
-functions_path=cwd+'/Functions.py'
-cov_Matrix=np.genfromtxt(cwd+'/C.txt')
+functions_path = cwd + '/Functions.py'
+cov_Matrix = np.genfromtxt(cwd+'/C.txt')
 
-ERE=Exp_rel_ent(cwd+'/D1_sample.txt',cwd+'/Functions.py',cov_Matrix,10)
-ere=ERE.Run(1000)
-print('Expected relative entropy is:',ere)
+ERE = Exp_rel_ent(cwd+'/D1_sample.txt', cwd+'/Functions.py', cov_Matrix, 10)
+ere = ERE.Run(1000)
+print('Expected relative entropy is:', ere)
 
 #Using paralle computation 
-pere=ERE.PRun(1000,3)
-print('Expected relative entropy is:',pere)
+pere = ERE.PRun(1000, 3)
+print('Expected relative entropy is:', pere)
