a17c778ef7173d9d3ad8af93febadf0550df85ca
Abolfazl Ahmadi-Rahmat <38430894+ahmadiphy@users.noreply.github.com>
2020-11-17 22:04:43 +0330

    Update example.py

--- Examples/Relative_entropy/example.py
+++ Examples/Relative_entropy/example.py
@@ -3,11 +3,11 @@ import sys
 import os
 cwd = os.getcwd()
 
-from MCKLD import Rel_ent,Exp_rel_ent
+from MCKLD import Rel_ent, Exp_rel_ent
 
 
-RE=Rel_ent(cwd + '/chain.txt',cwd + '/lnprior.txt',0.0,0.0)
-re=RE.Run()
+RE = Rel_ent(cwd + '/chain.txt', cwd + '/lnprior.txt', 0.0, 0.0)
+re = RE.Run()
 print(re)
 
 
